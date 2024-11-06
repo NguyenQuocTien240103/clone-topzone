ed46dfd679d339d52afb72edfe8fd00cf594ba72
NguyenQuocTien <nguyenquoctien2401@gmail.com>
2024-11-07 03:12:46 +0700

    update jquery

--- jquery/main.js
+++ jquery/main.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
   $("#header").load("layouts/header.html");
   $("#banner").load("sessions/banner.html");
+  $("footer").load("layouts/footer.html");
+  $("#cart-list").load("components/cart.html");
 });
